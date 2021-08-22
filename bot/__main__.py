4fe23dd93deedf2a7023effc0ee3579c63946178
Waris Ali <86531313+Devilboy04@users.noreply.github.com>
2021-08-22 10:46:41 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -55,8 +55,8 @@ This bot can mirror all your links to Google Drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Channel", f"{CHANNEL_LINK}")
-    buttons.buildbutton("Support Group", f"{SUPPORT_LINK}")
+    buttons.buildbutton("Channel", f"{t.me/Teammoviezcinemas}")
+    buttons.buildbutton("Support Group", f"{t.me/WarisZone}")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     LOGGER.info('UID: {} - UN: {} - MSG: {}'.format(update.message.chat.id, update.message.chat.username, update.message.text))
     uptime = get_readable_time((time.time() - botStartTime))
